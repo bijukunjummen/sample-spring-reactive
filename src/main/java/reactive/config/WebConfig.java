00ce1c3a5445408538609fcbae87e3843cc634da
Biju Kunjummen <biju.kunjummen@gmail.com>
2016-11-09 09:21:21 -0800

    Changed config to be compatible with latest snapshots

--- src/main/java/reactive/config/WebConfig.java
+++
@@ -1,8 +0,0 @@
-package reactive.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.WebReactiveConfiguration;
-
-@Configuration
-public class WebConfig extends WebReactiveConfiguration {
-}
